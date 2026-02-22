ab2ecb081604055746fd9aa73fdbf212316ee08a
JoachimHamraoui <joachimhamraoui@gmail.com>
2026-02-22 23:33:02 +0100

    deps: modules

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '8b007afa96f79334';
+  public const string VERSIONS_HASH = '348329d376f20e9b';
 
 }
